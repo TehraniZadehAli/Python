5f8e267871093f70b14a5fa8ad181f58e716a01c
TehraniZadehAli <62545740+TehraniZadehAli@users.noreply.github.com>
2020-03-23 23:25:21 +0430

    Update ChangeVars.py
    Adding comment

--- ChangeVars.py
+++ ChangeVars.py
@@ -1,13 +1,14 @@
+#Define a function for changing value of two variables
 def changevar(a,b):
-    a = a + b
-    b = a - b
-    a = a -b
-    print("a2 is : " ,a)
-    print("b2 is : ", b)
+    a = a + b   #Assign a+b to a
+    b = a - b   #Assign a-b to b
+    a = a -b    #Assign a-b to a
+    print("a2 is : " ,a)    #Printing new value for a
+    print("b2 is : " ,b)    #Printing new value for b
 
-a = int(input("a"))
+a = int(input("a"))     #Getting number from user
 b = int(input("b"))
-print(changevar(a,b))
+print(changevar(a,b))   #Call the function and print output
 
 
 
